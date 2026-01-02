88df176d104f11ad723eb8f6825e1dfa6583a866
dokumor8 <146167157+dokumor8@users.noreply.github.com>
2026-01-02 20:52:57 +0900

    selection

--- game_scene.gd
+++ game_scene.gd
@@ -1,8 +1,9 @@
 extends Node2D
 
 @onready var tilemap_layer: TileMapLayer = $TileMapLayer  # Adjust path
-@export var unit_scene: PackedScene = preload("res://unit.tscn")
-@export var unit_selection: PackedScene = preload("res://unit_selection.tscn")
+var UnitScene: PackedScene = preload("res://unit.tscn")
+var UnitSelectionScene: PackedScene = preload("res://unit_selection.tscn")
+var unit_selection: UnitSelection
 
 var selected_unit_marker: Sprite2D
 
@@ -17,52 +18,63 @@ var selected_unit: Unit
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	
-	for location in start_locations_1:
-		_spawn_unit_at_tile(unit_scene, location, 1)
-	for location in start_locations_2:
-		_spawn_unit_at_tile(unit_scene, location, 2)
+    
+    unit_selection = UnitSelectionScene.instantiate()
+    unit_selection.visible = false
+    add_child(unit_selection)
+    
+    for location in start_locations_1:
+        _spawn_unit_at_tile(UnitScene, location, 1)
+    for location in start_locations_2:
+        _spawn_unit_at_tile(UnitScene, location, 2)
 
 
 func _spawn_unit_at_tile(spawning_scene: PackedScene, grid_pos: Vector2i, side=1):
-	var unit = spawning_scene.instantiate()
-	unit.conflict_side = side
-	add_child(unit)
-	unit.grid_position = grid_pos
-	var local_pos = tilemap_layer.map_to_local(grid_pos)
-	var global_pos = tilemap_layer.to_global(local_pos)
-	unit.global_position = global_pos
+    var unit = spawning_scene.instantiate()
+    unit.conflict_side = side
+    add_child(unit)
+    unit.grid_position = grid_pos
+    var local_pos = tilemap_layer.map_to_local(grid_pos)
+    var global_pos = tilemap_layer.to_global(local_pos)
+    unit.global_position = global_pos
 
 
-func select_tile(coords: Vector2i) -> void:
-	selected_tile = coords
-	for unit: Unit in get_tree().get_nodes_in_group("units"):
-		if unit.grid_position == coords:
-			select_unit(unit)
-			return
+func select_tile(coords: Vector2i) -> bool:
+    selected_tile = coords
+    for unit: Unit in get_tree().get_nodes_in_group("units"):
+        if unit.grid_position == coords:
+            select_unit(unit)
+            return true
+    return false
 
 
 func select_unit(unit: Unit) -> void:
-	
-	selected_unit = unit
+    unit_selection.global_position = unit.global_position
+    unit_selection.visible = true
+    selected_unit = unit
 
 func _unhandled_input(event: InputEvent) -> void:
-	if event is InputEventMouseButton \
-	and event.button_index == MOUSE_BUTTON_LEFT \
-	and event.pressed:
-		
-		var local_pos: Vector2 = tilemap_layer.to_local(get_global_mouse_position())
-		var tile_coords: Vector2i = tilemap_layer.local_to_map(local_pos)
-		# Get the tile coordinates under the mouse (in one line!)
-		#var tile_coords: Vector2i = local_to_map(get_local_mouse_position())
-		
-		# Optional: Check if a tile actually exists there
-		if tilemap_layer.get_cell_source_id(tile_coords) != -1:
-			print("Selected tile at: ", tile_coords)  # Or emit a signal/store in a var
-			select_tile(tile_coords)  # Your turn-based logic here
+    if event is InputEventMouseButton \
+    and event.button_index == MOUSE_BUTTON_LEFT \
+    and event.pressed:
+        
+        var local_pos: Vector2 = tilemap_layer.to_local(get_global_mouse_position())
+        var tile_coords: Vector2i = tilemap_layer.local_to_map(local_pos)
+        
+        if tilemap_layer.get_cell_source_id(tile_coords) != -1:
+            print("Selected tile at: ", tile_coords)
+            var was_selected = select_tile(tile_coords)
+            if not was_selected:
+                clear_selection()
+            
+        else:
+            clear_selection()
 
+
+func clear_selection():
+    unit_selection.visible = false
 
 #func _unhandled_input(event: InputEvent) -> void:
-	# ... same if-condition ...
+    # ... same if-condition ...
 
-	# ... rest same ...
+    # ... rest same ...
